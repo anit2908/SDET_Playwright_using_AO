7ec693ffb9575c6115b3d368f962526ef14e33d7
anit2908 <maharanaanit@gmail.com>
2026-05-31 10:03:00 +0530

    Add Chapters 1-3 content and exercises

---
+++ Chapter_3/06_Identifiers Rules.js
@@ -0,0 +1,15 @@
+var $ = 10; // Valid identifier, can start with a dollar sign
+var _ =20; //  Valid identifier, can start with an underscore
+var p =10; // Valid identifier, can start with an underscore
+
+var ab =123;
+//var 123 = 123; // Invalid identifier, cannot start with a number
+//var my-name = "Anit"; // Invalid identifier, cannot contain hyphens
+//var function = "Hello"; // Invalid identifier, cannot use reserved keywords
+
+var Anit_Maharana = "Hello";
+var Anit$Maharana = "Hello";
+var Anit123213 = "Hello";
+
+
+    
---
+++ Chapter_3/08_Comments.js
@@ -0,0 +1,20 @@
+//Single line commnent
+
+/*
+Multi line comment
+*/
+
+/**
+multi line comment
+**/
+
+/**
+var a =10;
+console.log(a);
+**/
+
+var b = 20;
+console.log(b);
+
+var c = b;
+console.log(c);
